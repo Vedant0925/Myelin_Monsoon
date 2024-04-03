9ad80c2970ab3696418580ea4f6a3a879f620f85
Vedant Patil <74593220+Vedant0925@users.noreply.github.com>
2024-04-03 17:34:56 +0530

    Create model.py

---
+++ RollNumber_Detection/Single Digit/model.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0  # Normalize to [0, 1]
+
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+model = models.Sequential([
+    layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+model.save('mnist_model.h5')
